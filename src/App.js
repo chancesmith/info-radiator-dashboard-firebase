43d7ec70bf9d4312b710b7a68440d55edbd29109
Chance Smith <chancesmithb@gmail.com>
2017-11-06 22:54:24 -0600

    moves config into App.js and edits the env example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
 
+const config = {
+  "apiKey": process.env.REACT_APP_firebase_apiKey,
+  "authDomain": process.env.REACT_APP_firebase_authDomain,
+  "databaseURL": process.env.REACT_APP_firebase_databaseURL,
+  "projectId": process.env.REACT_APP_firebase_projectId,
+  "storageBucket": process.env.REACT_APP_firebase_storageBucket,
+  "messagingSenderId": process.env.REACT_APP_firebase_messagingSenderId
+};
+
+firebase.initializeApp(config);
+
 class App extends Component {
   constructor() {
     super();
@@ -15,6 +26,7 @@ class App extends Component {
     const rootRef = firebase.database().ref().child('dashboard');
     const usersCount = rootRef.child('usersCount');
     const countiesCount = rootRef.child('countiesReachedCount');
+    console.log(rootRef, usersCount, countiesCount);
     usersCount.on('value', snap => {
       this.setState({
         visits: snap.val()
--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  "apiKey": process.env.REACT_APP_firebase_apiKey,
-  "authDomain": process.env.REACT_APP_firebase_authDomain,
-  "databaseURL": process.env.REACT_APP_firebase_databaseURL,
-  "storageBucket": process.env.REACT_APP_firebase_storageBucket
-};
